e7b5731a763310adc92eefa8ab85e152ed9c88f6
Sushovan Ghosh <sushovan1908@gmail.com>
2024-08-31 14:35:37 +0530

    hw rj but only int type

---
+++ area_of_circle_rj_hw.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+ 
+    int r=0;
+    int py=3;
+    int area=0;
+    
+    printf("enter the radius:");
+    scanf("%d", &r);
+    
+    area= py*r*r;
+    
+    printf("the area is: %d", area);
+    
+
+}
