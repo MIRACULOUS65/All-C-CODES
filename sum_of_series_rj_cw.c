e49650bd763febaf436648bff007bc4d17dac83d
Sushovan Ghosh <sushovan1908@gmail.com>
2024-09-07 14:35:00 +0530

    sum of a series

---
+++ sum_of_series_rj_cw.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int sum=0;
+    int n;
+
+    printf("enter the number to which u want the sum");
+    scanf("%d",&n);
+
+    for ( i=0;i<n; i++)
+    
+    {
+        sum=sum+i;
+    }
+    
+    printf("the sum is:%d ",sum);
+}
