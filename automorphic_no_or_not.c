6d8b03e2fa3216a82d83f8fd89d94e826e49902d
Sushovan Ghosh <sushovan1908@gmail.com>
2024-09-16 21:12:23 +0530

    rj hw but not completed yet

---
+++ automorphic_no_or_not.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,square;
+    
+    printf("enter the number");
+    scanf("%d",&n);
+    
+    square=n*n;
+    
+    // don't know how yet but we can do something using array
+}
