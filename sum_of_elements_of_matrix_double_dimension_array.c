aaa2adebc8831e798821466e411650523481a8fc
Sushovan Ghosh <sushovan1908@gmail.com>
2024-09-28 20:04:01 +0530

    first programme for array in double dimension

---
+++ sum_of_elements_of_matrix_double_dimension_array.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int m, sum = 0, i, j;
+
+    printf("enter the range of the matrix");
+    scanf("%d", &m);
+
+    int arr[m][m];
+
+    printf("enter the elements into the array");
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("the entered matrix is:\n");
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            printf("%d \t", arr[i][j]);
+        }
+        printf("\n");
+    }
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            sum = sum + arr[i][j];
+        }
+    }
+
+    printf("the sum is:%d", sum);
+}
