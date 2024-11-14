b8db782c8e20175412d0ff4c539e37627cbf1497
Sushovan Ghosh <sushovan1908@gmail.com>
2024-11-15 00:29:09 +0530

    recursion

---
+++ Consecutive_occurance_of_a_Charecter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char s[100];
+
+    printf("enter the words for the string of which u eant to check the no. of consecutive occurance of a charecter");
+    scanf("%s",s);
+
+    
+}
