eb165daf8cc491c0c19a29d8f38d425212e192f3
Sushovan Ghosh <sushovan1908@gmail.com>
2024-11-16 01:15:37 +0530

    pointer 1st problem

--- pointers_#1_problem.c
+++ pointers_#1_problem.c
@@ -11,7 +11,7 @@ int main()
     pntr= &a;
     printf("value of the enter pointer is:%d \n", *pntr); //to print the value of the pointer
     printf("The Address of pointer is %p\n", &pntr ); //to print the address of the pointer
-	printf("The Address of a is %p\n", a); // to print the address of the variable a
+	printf("The Address of a is %p\n", a); // to print the address of the variable a typically in hexadecimal format. The %p format specifier is specifically used to print pointer values.
 	printf("The Address of pointer in another way without using & and by using percent p instead of percent d is %p\n",pntr ); // to print the address of the pointer in another way
     printf("The Value of a is %d\n", *pntr );
 
