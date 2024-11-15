502601158d8322aa75f1a50063ae3cd99be0a269
Sushovan Ghosh <sushovan1908@gmail.com>
2024-11-16 01:04:24 +0530

    pointer 1st problem

---
+++ pointers_#1_problem.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+
+    printf("enter the number:\n");
+    scanf("%d",&a);
+
+    int* pntr;
+
+    pntr= &a;
+    printf("value of the enter pointer is:%d \n", *pntr); //to print the value of the pointer
+    printf("The Address of pointer is %p\n", &pntr ); //to print the address of the pointer
+	printf("The Address of a is %p\n", a); // to print the address of the variable a
+	printf("The Address of pointer in another way without using & and by using percent p instead of percent d is %p\n",pntr ); // to print the address of the pointer in another way
+    printf("The Value of a is %d\n", *pntr );
+
+
+    return 0;
+}
