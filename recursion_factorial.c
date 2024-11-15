0cbae42aaa3c08d08245b03b0852577c759dc5b4
Sushovan Ghosh <sushovan1908@gmail.com>
2024-11-15 12:24:12 +0530

    recursion

--- recursion_factorial.c
+++ recursion_factorial.c
@@ -1,1 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+int factorial(int x)
+{
+    if (x == 1 || x == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (x * factorial(x - 1));
+    }
+}
+
+int main()
+{
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    int fac = factorial(number);
+    printf("the factorial value of the number %d is:%d", number, fac);
+}
